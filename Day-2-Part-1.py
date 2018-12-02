5e94a4c90189bedcaea448bdfa7bcc162196a4db
daniel_iova <32776745+TheVigilante39@users.noreply.github.com>
2018-12-02 18:48:40 +0200

    Create Day-2-Part-1.py

---
+++ Day-2-Part-1.py
@@ -0,0 +1,32 @@
+import os
+
+count2 = 0
+count3 = 0
+
+def generate_arr(a):
+    i = 97
+    while (i != 123):
+        a.append(word.count(chr(i)))
+        print (chr(i))
+        i+=1
+
+with open('in2.txt') as f:
+    for word in f:
+        c2 = 0
+        c3 = 0
+        a = []
+        generate_arr(a)
+        for val in a:
+            if (val == 2):
+                c2+=1
+            elif(val == 3):
+                c3+=1
+        if (c2 != 0 and c3 == 0):
+            count2 += 1
+        elif (c3 != 0 and c2 == 0):
+            count3 += 1
+        elif (c2!= 0 and c3!= 0):
+            count2 += 1
+            count3 += 1
+
+print (count2*count3)
