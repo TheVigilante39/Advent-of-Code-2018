93631403697622e18fb7034115c87a6707764cef
daniel_iova <32776745+TheVigilante39@users.noreply.github.com>
2018-12-03 14:33:35 +0200

    Update and rename Day-3-Part-1.py to Day-3-Full.py

--- Day-3-Full.py
+++ Day-3-Full.py
@@ -1,4 +1,5 @@
 import re
+
 a=[]
 def make_matrix(matrix):
     row=[]
@@ -10,8 +11,9 @@ def make_matrix(matrix):
 
 make_matrix(a)
 
+#------------------------------Part 1 Start --------------------------------
 lines = []
-with open("in.txt") as f:
+with open("in3.txt") as f:
     for line in f:
         lines.append(re.split('[@# ,:x\n,\t]', line)) #1,4,5,7,8
 
@@ -30,5 +32,18 @@ for i in range(len(a)):
     for j in range(len(a)):
         if (a[i][j] == 'X'):
             count +=1
-print (count)
-    
+print ("Part 1: %d" %count)
+#------------------------------Part 1 End-----------------------------------
+
+
+#------------------------------Part 2 Start---------------------------------
+for i in range(len(lines)):
+    counter = 0
+    for k in range (int(lines[i][4]), int(lines[i][4])+int(lines[i][7])):
+        for l in range (int(lines[i][5]), int(lines[i][5])+int(lines[i][8])):
+            if (a[k][l] == '#'):
+                counter+=1
+    if (counter == int(lines[i][7])*int(lines[i][8])):
+        print ("Part 2: %s" %lines[i][1])
+        break
+#------------------------------Part 2 End-----------------------------------
